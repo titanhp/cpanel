15212de9cf86129caaba634159bc9d2df13181ad
TiTanHp <titanhp@users.noreply.github.com>
2017-05-19 08:13:49 +0700

    Create restart_all_service.sh

---
+++ restart_all_service.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Restart all service by Thanh LH"
+echo "WAITING..."
+process=( http mysql nginx php )
+services=( mysql httpd nginx )
+for i in "${process[@]}"
+do
+    echo Kill $i
+    kill -9 $(ps aux | grep -e $i | awk '{ print $2 }')
+done
+
+for j in "${services[@]}"
+do
+    echo Start $j
+    /etc/init.d/$j start
+done
