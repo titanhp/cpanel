378a622babab34df75b2f2b538ae3f465a9a8f32
TiTanHp <titanhp@users.noreply.github.com>
2017-06-23 14:00:27 +0700

    Create loadaverage.sh

---
+++ script/loadaverage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CPU_LOAD=$(uptime | cut -d"," -f4 | cut -d":" -f2 | cut -d" " -f2 | sed -e "s/\.//g")
+CPU_THRESHOLD=700
+
+if [ $CPU_LOAD -gt $CPU_THRESHOLD ] ; then
+  kill -9 $(ps -eo pid | sort -k 1 -r | grep -v PID | head -n 1)
+fi
+
+exit 0
