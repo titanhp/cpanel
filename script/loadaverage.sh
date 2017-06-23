e00b355cf06c3d654f94823aeb4d00bcfe90fb3e
TiTanHp <titanhp@users.noreply.github.com>
2017-06-23 14:15:55 +0700

    Update loadaverage.sh

--- script/loadaverage.sh
+++ script/loadaverage.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 while [ 1=1 ];do
-      CPU_LOAD=$(uptime | cut -d"," -f4 | cut -d":" -f2 | cut -d" " -f2 | sed -e "s/\.//g")
-      CPU_THRESHOLD=700
-      echo CPU_LOAD
-      sleep 5
+    CPU_LOAD=$(uptime | cut -d"," -f4 | cut -d":" -f2 | cut -d" " -f2 | sed -e "s/\.//g")
+    CPU_THRESHOLD=680
+	echo $CPU_LOAD
+	if [ $CPU_LOAD -gt $CPU_THRESHOLD ] ; then
+		echo OverLoad
+		kill -9 $(ps aux | grep -ie php | awk '{ print $2 }')
+	fi
+    sleep 5
 done
