5a86fb0ae2137a0fcbffe6d796d2c2cd5346cd3c
TiTanHp <titanhp@users.noreply.github.com>
2017-06-23 14:05:18 +0700

    Update loadaverage.sh

--- script/loadaverage.sh
+++ script/loadaverage.sh
@@ -2,9 +2,6 @@
 while [ 1=1 ];do
       CPU_LOAD=$(uptime | cut -d"," -f4 | cut -d":" -f2 | cut -d" " -f2 | sed -e "s/\.//g")
       CPU_THRESHOLD=700
-
-      if [ $CPU_LOAD -gt $CPU_THRESHOLD ] ; then
-        kill -9 $(ps -eo pid | sort -k 1 -r | grep -v PID | head -n 1)
-      fi
+      echo CPU_LOAD
       sleep 5
 done
