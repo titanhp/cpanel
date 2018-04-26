7c6b6c60c577c4e0079f4ff960274196e86f663e
TiTanHp <titanhp@users.noreply.github.com>
2018-04-26 10:09:26 +0700

    Update delmailqueue_background.sh

--- script/delmailqueue_background.sh
+++ script/delmailqueue_background.sh
@@ -2,11 +2,13 @@
 while [ 1 = 1 ]
 do
    tong=$(/usr/sbin/exim -bpc)
-   #echo $tong
-   if [ $tong -ge 50000 ]
+   echo $tong
+   if [ $tong -ge 10000 ]
    then
-      /usr/sbin/exim -bp | awk '/^ *[0-9]+[mhd]/{print "exim -Mrm " $3}' | bash
-      echo "Mail queue con lai:"
-      /usr/sbin/exim -bpc
+      #/usr/sbin/exim -bp | awk '/^ *[0-9]+[mhd]/{print "exim -Mrm " $3}' | bash
+	   /usr/sbin/exim -bp | exiqgrep -i | xargs exim -Mrm
+      #echo "Mail queue con lai:"
+      #/usr/sbin/exim -bpc
    fi
+sleep 30
 done
